6344c75c32763de14588840ee01eee5df86dfbbc
Petey <pdotjpr@gmail.com>
2017-01-20 20:02:27 -0800

    Added new drive modes to Driver

--- src/org/usfirst/frc/team1717/controllers/Driver.java
+++ src/org/usfirst/frc/team1717/controllers/Driver.java
@@ -6,6 +6,9 @@ import org.usfirst.frc.team1717.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj.Joystick;
 import utils.ArcadeDriveCalculator;
+import utils.DriveCalculator;
+import utils.RadiusDriveCalculator;
+import utils.TankDriveCalculator;
 import utils.Vector2d;
 import utils.Vectors;
 
@@ -17,30 +20,64 @@ public class Driver {
 	
 	
 	public enum DriveMode{
-		POWER, VELOCITY
+		POWER, VELOCITY, STOPPED
 	}
 	
-	public DriveMode mode;
+	public enum ControlMode{
+		ARCADE, TANK, RADIUS, QUICK_RADIUS
+	}
+	
+	public DriveMode dMode;
+	public ControlMode cMode;
+	private DriveCalculator driveCalc;
 	
 	public Driver() {
 		JS_FW_BACK = new Joystick(BotConstants.JS_FW_BACK);
 		JS_TURN = new Joystick(BotConstants.JS_TURN);
 		train = Robot.runningrobot.driveTrain;
-		mode = DriveMode.POWER;
+		dMode = DriveMode.POWER;
+		cMode = ControlMode.ARCADE;
+		driveCalc = ArcadeDriveCalculator.INSTANCE;
 	}
 	
 	public void teleopPeriodic(){
+		handleCalc();
 		drive();
 	}
 	
 	public void drive(){
 		Vector2d driveVector = Vectors.fromJoystick(JS_FW_BACK, JS_TURN, true);
-		if(mode == DriveMode.POWER){
-		    Vector2d arcadeVector = ArcadeDriveCalculator.INSTANCE.compute(driveVector);
-		    train.setPowerLeftRight(arcadeVector);
-		}
-		else{
-		    train.brake();
+		Vector2d controlVector = driveCalc.compute(driveVector);
+        switch(dMode){
+            case POWER:
+            	train.setPowerLeftRight(controlVector);
+            	break;
+            case VELOCITY:
+            	train.setVelocityLeftRight(controlVector.scale(BotConstants.ROBOT_MAX_VELOCITY));
+            	break;
+            default:
+            	train.brake();
+            	break;
+        }
+	}
+	
+	public void handleCalc(){
+		switch(cMode){
+		    case ARCADE:
+		    	driveCalc = ArcadeDriveCalculator.INSTANCE;
+		    	break;
+		    case TANK:
+		    	driveCalc = TankDriveCalculator.INSTANCE;
+		    	break;
+		    case RADIUS:
+		    	driveCalc = RadiusDriveCalculator.INSTANCE;
+		    	((RadiusDriveCalculator) driveCalc).setQuick(false);
+		    	break;
+		    case QUICK_RADIUS:
+		    	driveCalc = RadiusDriveCalculator.INSTANCE;
+		    	((RadiusDriveCalculator) driveCalc).setQuick(true);
+		    default:
+		    	driveCalc = ArcadeDriveCalculator.INSTANCE;
 		}
 	}
 }
