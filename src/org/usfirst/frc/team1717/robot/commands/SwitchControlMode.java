231ac5ac03a39382971dc3a9804fdb53f93e8857
Petey <pdotjpr@gmail.com>
2017-01-20 19:42:04 -0800

    Fixed some post-conflict drama

--- src/org/usfirst/frc/team1717/robot/commands/SwitchControlMode.java
+++ src/org/usfirst/frc/team1717/robot/commands/SwitchControlMode.java
@@ -6,29 +6,35 @@ import org.usfirst.frc.team1717.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SwitchControlMode extends Command{
-	
-	Driver driver;
-	Driver.ControlMode targetMode;
-	private boolean isDone = false;
-	
-	public SwitchControlMode(Driver.ControlMode mode){
-		driver = Robot.runningrobot.driver;
-		targetMode = mode;
-	}
-	
-	@Override
-	protected void initialize(){
-		if(!driver.cMode.equals(Driver.ControlMode.RADIUS) && targetMode.equals(Driver.ControlMode.QUICK_RADIUS)){
-			isDone = true;
-			return;
-		}
-		driver.cMode = targetMode;
-		isDone = true;
-	}
-	
+
 	@Override
 	protected boolean isFinished() {
-		return isDone;
+		// TODO Auto-generated method stub
+		return false;
 	}
+	
+//	Driver driver;
+//	Driver.ControlMode targetMode;
+//	private boolean isDone = false;
+//	
+//	public SwitchControlMode(Driver.ControlMode mode){
+//		driver = Robot.runningrobot.driver;
+//		targetMode = mode;
+//	}
+//	
+//	@Override
+//	protected void initialize(){
+//		if(!driver.cMode.equals(Driver.ControlMode.RADIUS) && targetMode.equals(Driver.ControlMode.QUICK_RADIUS)){
+//			isDone = true;
+//			return;
+//		}
+//		driver.cMode = targetMode;
+//		isDone = true;
+//	}
+//	
+//	@Override
+//	protected boolean isFinished() {
+//		return isDone;
+//	}
 
 }
--- src/org/usfirst/frc/team1717/robot/commands/SwitchDriveMode.java
+++ src/org/usfirst/frc/team1717/robot/commands/SwitchDriveMode.java
@@ -6,25 +6,31 @@ import org.usfirst.frc.team1717.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class SwitchDriveMode extends Command{
-	
-	Driver driver;
-	Driver.DriveMode targetMode;
-	private boolean isDone = false;
-	
-	public SwitchDriveMode(Driver.DriveMode mode){
-		driver = Robot.runningrobot.driver;
-		targetMode = mode;
-	}
-	
-	@Override
-	protected void initialize(){
-		driver.dMode = targetMode;
-		isDone = true;
-	}
-	
+
 	@Override
 	protected boolean isFinished() {
-		return isDone;
+		// TODO Auto-generated method stub
+		return false;
 	}
+	
+//	Driver driver;
+//	Driver.DriveMode targetMode;
+//	private boolean isDone = false;
+//	
+//	public SwitchDriveMode(Driver.DriveMode mode){
+//		driver = Robot.runningrobot.driver;
+//		targetMode = mode;
+//	}
+//	
+//	@Override
+//	protected void initialize(){
+//		driver.dMode = targetMode;
+//		isDone = true;
+//	}
+//	
+//	@Override
+//	protected boolean isFinished() {
+//		return isDone;
+//	}
 
 }
