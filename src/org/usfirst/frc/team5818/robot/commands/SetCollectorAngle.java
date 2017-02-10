92fcce0e296f44e0efa66eb78585a4fa777cd02c
Albert Miao <amiao541769@gmail.com>
2017-02-09 20:41:34 -0800

    Command for setting the collector angle

---
+++ src/org/usfirst/frc/team5818/robot/commands/SetCollectorAngle.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team5818.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+import org.usfirst.frc.team5818.robot.Robot;
+import org.usfirst.frc.team5818.robot.subsystems.Collector;
+
+public class SetCollectorAngle extends Command {
+    
+    private Collector collector;
+    double targetAng;
+    public static final double TOLERANCE = 3;
+    
+    public SetCollectorAngle(double angle) {
+        collector = Robot.runningrobot.collector;
+        targetAng = angle;
+    }
+    
+    public void initialize(){
+        collector.getanglePID().setAbsoluteTolerance(TOLERANCE);
+        collector.getanglePID().setSetpoint(targetAng);
+    }
+    
+    @Override
+    public void execute(){
+        
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return collector.getanglePID().onTarget();
+    }
+    
+    @Override
+    public void end(){
+        collector.stop();
+    }
+    
+    @Override
+    public void interrupted(){
+        collector.stop();
+    }
+}
