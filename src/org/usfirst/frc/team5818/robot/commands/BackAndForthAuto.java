861d5996c0bdb93ba3315a7220b3a87c73c322c7
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-02-11 15:00:54 -0800

    actually the command this time, i forgot to save

--- src/org/usfirst/frc/team5818/robot/commands/BackAndForthAuto.java
+++ src/org/usfirst/frc/team5818/robot/commands/BackAndForthAuto.java
@@ -1,6 +1,17 @@
 package org.usfirst.frc.team5818.robot.commands;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
-public class BackAndForthAuto {
-
+public class BackAndForthAuto extends CommandGroup {
+    
+    private double distance;
+    private double powerCap;
+    
+    public BackAndForthAuto(double dist, double pow) {
+        distance = dist;
+        powerCap = pow;
+        this.addSequential(new DriveStraight(dist, pow, 1.8, DriveStraight.Camera.CAM_FORWARD, true));
+        this.addSequential(new DriveStraight(dist, -pow, 1.8, DriveStraight.Camera.CAM_BACKWARD, true));
+    }
+    
 }
