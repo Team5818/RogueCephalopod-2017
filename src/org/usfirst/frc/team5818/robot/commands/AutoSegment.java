a8799bce931ca649b22bd7e8a9122374938426a9
Petey <pdotjpr@gmail.com>
2017-02-12 19:08:56 -0800

    Just driving for now
    TODO:
    Add collector arm, gear deployment, gear collection

--- src/org/usfirst/frc/team5818/robot/commands/AutoSegment.java
+++ src/org/usfirst/frc/team5818/robot/commands/AutoSegment.java
@@ -4,32 +4,32 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class AutoSegment extends CommandGroup{
 
-    
-    private DriveStraight overshoot;
-    private DriveStraight visionCorrection;
-    private DriveStraight finalStretch;
+    private CommandGroup drive;
+    private DriveStraight driveOvershoot;
+    private DriveStraight driveVision;
+    private DriveStraight driveFinal;
     
     public enum Direction{
         BACKWARD, FORWARD
     }
     
     public AutoSegment(Direction dir){
+        drive = new CommandGroup();
         if(dir.equals(Direction.BACKWARD)){
-            overshoot = new DriveStraight(24, -.4, 1.8, false);
-            visionCorrection = new DriveStraight(36, -.4, 2.0, DriveStraight.Camera.CAM_BACKWARD, false);
-            finalStretch = new DriveStraight(12, -.4, 1.0, true);
+            driveOvershoot = new DriveStraight(24, -.4, 1.8, false);
+            driveVision = new DriveStraight(36, -.4, 2.0, DriveStraight.Camera.CAM_BACKWARD, false);
+            driveFinal = new DriveStraight(12, -.4, 1.0, true);
         }
         else{
-            overshoot = new DriveStraight(24, .4, 1.8, false);
-            visionCorrection = new DriveStraight(36, .4, 2.0, DriveStraight.Camera.CAM_FORWARD, false);
-            finalStretch = new DriveStraight(12, .4, 1.0, true);
+            driveOvershoot = new DriveStraight(24, .4, 1.8, false);
+            driveVision = new DriveStraight(36, .4, 2.0, DriveStraight.Camera.CAM_FORWARD, false);
+            driveFinal = new DriveStraight(12, .4, 1.0, true);
         }
-    }
-    
-    @Override
-    protected boolean isFinished() {
-        // TODO Auto-generated method stub
-        return false;
+        drive.addSequential(driveOvershoot);
+        drive.addSequential(driveVision);
+        drive.addSequential(driveFinal);
+        
+        this.addSequential(drive);
     }
 
 }
