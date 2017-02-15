fe8b0bae9fcd0c35984f413a6850b5cc23d639fa
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-02-14 21:43:15 -0800

    the merge master knows all

--- src/org/usfirst/frc/team5818/robot/subsystems/CameraController.java
+++ src/org/usfirst/frc/team5818/robot/subsystems/CameraController.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class CameraController extends Subsystem{
 
-    private enum Camera{
+    public enum Camera{
         FRONT, BACK
     }
     
