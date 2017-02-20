c771021d4f39f6c51212a4e8b0208058d1d5de91
Petey <pdotjpr@gmail.com>
2017-02-20 12:42:52 -0800

    Sanic on collector

--- src/org/usfirst/frc/team5818/robot/subsystems/CollectorRollers.java
+++ src/org/usfirst/frc/team5818/robot/subsystems/CollectorRollers.java
@@ -1,53 +1,64 @@
 package org.usfirst.frc.team5818.robot.subsystems;
 
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team5818.robot.RobotMap;
 import org.usfirst.frc.team5818.robot.constants.BotConstants;
 
-import com.ctre.CANTalon;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.command.Subsystem;
-
 public class CollectorRollers extends Subsystem {
-
+    
     private CANTalon topRoller;
     private CANTalon botRoller;
-    private DigitalInput lineBreak;
-
+    private Ultrasonic sanic;
+    
     public CollectorRollers() {
         topRoller = new CANTalon(RobotMap.TOP_COLLECTOR_ROLLER);
         botRoller = new CANTalon(RobotMap.BOT_COLLECTOR_ROLLER);
-        lineBreak = new DigitalInput(2);
+        sanic = new Ultrasonic(2,3);
+        enableSanic();
     }
-
+    
     public void setTopPower(double x) {
         topRoller.set(x * BotConstants.MAX_POWER);
     }
-
+    
     public void setBotPower(double x) {
         botRoller.set(x * BotConstants.MAX_POWER);
     }
-
+    
     public void stop() {
         topRoller.set(0);
         botRoller.set(0);
     }
-
+    
     public double getTopCurrent() {
         return topRoller.getOutputCurrent();
     }
-
+    
     public double getBotCurrent() {
         return botRoller.getOutputCurrent();
     }
-
-    public boolean receivingBeam() {
-        return lineBreak.get();
+    
+    public Ultrasonic getSanic(){
+        return sanic;
+    }
+    
+    public void enableSanic(){
+        sanic.setAutomaticMode(true);
+    }
+    
+    
+    public double readSanic(){
+        return sanic.getRangeInches();
     }
 
     @Override
     protected void initDefaultCommand() {
-
+        
     }
 
 }
