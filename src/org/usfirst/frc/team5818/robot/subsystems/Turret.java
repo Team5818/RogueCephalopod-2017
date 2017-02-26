aadc90f76605af10fc1ca6d3a53e53a66cb36bf6
Petey <pdotjpr@gmail.com>
2017-02-25 16:18:04 -0800

    Int math error.

--- src/org/usfirst/frc/team5818/robot/subsystems/Turret.java
+++ src/org/usfirst/frc/team5818/robot/subsystems/Turret.java
@@ -22,7 +22,7 @@ public class Turret extends Subsystem implements PIDSource, PIDOutput {
     public static final double kD = 0.0;
 
     public static final double CENTER_OFFSET = Preferences.getInstance().getDouble("TurretCenter", 1920);
-    public static final double POT_SCALE = Preferences.getInstance().getDouble("TurretScale", -90/100);
+    public static final double POT_SCALE = Preferences.getInstance().getDouble("TurretScale", -90.0/100.0);
 
     private CANTalon motor;
 
@@ -91,11 +91,11 @@ public class Turret extends Subsystem implements PIDSource, PIDOutput {
 
     @Override
     public void pidWrite(double x) {
-//        if (getAngle() > 100) {
-//            x = Math.min(x, 0);
-//        } else if (getAngle() < -100) {
-//            x = Math.max(0, x);
-//        }
+        if (getAngle() > 100) {
+            x = Math.min(x, 0);
+        } else if (getAngle() < -100) {
+            x = Math.max(0, x);
+        }
         motor.set(x);
     }
 
