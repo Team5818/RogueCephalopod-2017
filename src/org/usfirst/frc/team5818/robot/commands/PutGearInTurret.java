74c0ff46024eda68e1b039490933b8664f563a6d
JacksonMcLaughlin <jacksonmclaughlin1@gmail.com>
2017-03-08 11:33:23 -0800

    actual command

--- src/org/usfirst/frc/team5818/robot/commands/PutGearInTurret.java
+++ src/org/usfirst/frc/team5818/robot/commands/PutGearInTurret.java
@@ -1,5 +1,26 @@
 package org.usfirst.frc.team5818.robot.commands;
 
-public class PutGearInTurret {
+import org.usfirst.frc.team5818.robot.Robot;
+import org.usfirst.frc.team5818.robot.subsystems.Collector;
+import org.usfirst.frc.team5818.robot.subsystems.CollectorRollers;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class PutGearInTurret extends CommandGroup {
+	CollectorRollers roll = Robot.runningRobot.roll;
+	public PutGearInTurret() {
+		this.addSequential(new TurretMoveToZero());
+		this.addSequential(new SetCollectorAngle(Collector.LOAD_POSITION));
+		this.addSequential(new SetCollectorPower(false, 0.7, 1000));
+		requires(Robot.runningRobot.collector);
+		requires(Robot.runningRobot.roll);
+		requires(Robot.runningRobot.turret);
+		setInterruptible(true);
+	}
+	@Override
+	protected void end() {
+		roll.setBotPower(0);
+		roll.setTopPower(0);
+	}
 
 }
