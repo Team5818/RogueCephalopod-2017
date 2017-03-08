597693152d4452082c16b2ca1d5170fc6c56b4af
Petey <pdotjpr@gmail.com>
2017-03-08 12:08:57 -0800

    exterminated

--- src/org/usfirst/frc/team5818/robot/RobotMap.java
+++ src/org/usfirst/frc/team5818/robot/RobotMap.java
@@ -36,8 +36,6 @@ public class RobotMap {
 
     public static final int TURRET_EXTENDER_SOLENOID = 1;
     public static final int TURRET_PUNCHER_SOLENOID = 2;
-    public static final int LEFT_MINI_SOLENOID = 3;
-    public static final int RIGHT_MINI_SOLENOID = 4;
     public static final int SHIFTER_SOLENOID = 5;
     public static final int LED_SOLENOID = 6;
 
