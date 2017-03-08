f49e0a4c6cd3cddc2c6869339fb6c3e5803dcb50
JacksonMcLaughlin <jacksonmclaughlin1@gmail.com>
2017-03-08 11:37:35 -0800

    no requires, no turretmovetozero

--- src/org/usfirst/frc/team5818/robot/commands/PutGearInTurret.java
+++ src/org/usfirst/frc/team5818/robot/commands/PutGearInTurret.java
@@ -9,12 +9,10 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class PutGearInTurret extends CommandGroup {
 	CollectorRollers roll = Robot.runningRobot.roll;
 	public PutGearInTurret() {
-		this.addSequential(new TurretMoveToZero());
+		this.addSequential(new SetTurretAngle(0));
 		this.addSequential(new SetCollectorAngle(Collector.LOAD_POSITION));
 		this.addSequential(new SetCollectorPower(false, 0.7, 1000));
-		requires(Robot.runningRobot.collector);
 		requires(Robot.runningRobot.roll);
-		requires(Robot.runningRobot.turret);
 		setInterruptible(true);
 	}
 	@Override
@@ -22,5 +20,4 @@ public class PutGearInTurret extends CommandGroup {
 		roll.setBotPower(0);
 		roll.setTopPower(0);
 	}
-
 }
