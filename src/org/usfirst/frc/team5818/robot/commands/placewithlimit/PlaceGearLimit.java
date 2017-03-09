cc31746b0cc8f5719af701b763db75ced45e36a0
Kenzie Togami <ket1999@gmail.com>
2017-03-08 23:20:26 -0800

    :moon: i am the sleep

--- src/org/usfirst/frc/team5818/robot/commands/placewithlimit/PlaceGearLimit.java
+++ src/org/usfirst/frc/team5818/robot/commands/placewithlimit/PlaceGearLimit.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team5818.robot.commands.placewithlimit;
 
+import org.usfirst.frc.team5818.robot.Robot;
 import org.usfirst.frc.team5818.robot.commands.SetExtendTurret;
 import org.usfirst.frc.team5818.robot.commands.SetPunchTurret;
+import org.usfirst.frc.team5818.robot.commands.TurretSmallAdjustment;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.TimedCommand;
@@ -14,6 +16,18 @@ public class PlaceGearLimit extends CommandGroup {
         this.addSequential(new TimedCommand(0.6));
         this.addSequential(new SetExtendTurret(false));
         this.addSequential(new SetPunchTurret(false));
+    }
+
+    @Override
+    protected void end() {
+        if (Math.abs(Robot.runningRobot.turret.getAngle()) < 10) {
+            new TurretSmallAdjustment(0).start();
+        }
+    }
+
+    @Override
+    protected void interrupted() {
+        end();
     }
 
 }
