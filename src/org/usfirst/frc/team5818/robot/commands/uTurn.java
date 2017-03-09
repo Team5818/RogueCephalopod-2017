f42824775898c722baa04b61892207fc3dce10ec
Jackson McLaughlin <jacksonmclaughlin1@gmail.com>
2017-03-09 09:34:25 -0800

    added uTurn

---
+++ src/org/usfirst/frc/team5818/robot/commands/uTurn.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team5818.robot.commands;
+
+import org.usfirst.frc.team5818.robot.commands.driveatratio.DriveAtRatio;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class uTurn extends CommandGroup {
+	private static final double BOT_WIDTH = 32.0; // guess
+	public enum Side {
+		LEFT,
+		RIGHT
+	}
+	private DriveAtRatio turn;
+	
+	public uTurn(double insideRadius, double ratio, double power, Side side) {
+		turn = DriveAtRatio.withDeadReckon(t -> {
+			if (side == Side.LEFT) {
+				t.targetRatio(((BOT_WIDTH + insideRadius) / insideRadius));
+			} else {
+				t.targetRatio(insideRadius / (BOT_WIDTH + insideRadius));
+			}
+			t.inches(insideRadius + (BOT_WIDTH / 2) * Math.PI);
+			t.maxPower(-1 * power);
+			t.stoppingAtEnd(false);
+		});
+		this.addSequential(turn);
+	}
+		
+}
