07a65e2caf6f555cd788c7b522489e0ef4bd16ed
Petey <pdotjpr@gmail.com>
2017-03-08 18:46:59 -0800

    Added spinning headband on cod river override

--- src/org/usfirst/frc/team5818/robot/commands/OverrideControlCommand.java
+++ src/org/usfirst/frc/team5818/robot/commands/OverrideControlCommand.java
@@ -27,7 +27,7 @@ public class OverrideControlCommand extends ControlCommand {
     protected void setPower() {
         Vector2d driveVector = Vectors.fromJoystick(js, true);
         driveVector = MathUtil.adjustDeadband(driveVector, Driver.DEADBAND_VEC);
-        RadiusDriveCalculator.INSTANCE.setQuick(Math.abs(js.getTwist()) > Driver.TWIST_DEADBAND);
+        RadiusDriveCalculator.INSTANCE.setQuick(Math.abs(js.getTwist()) > Driver.TWIST_DEADBAND || driveVector.getY() == 0);
         Vector2d controlVector = driver.driveCalc.compute(driveVector);
         switch (driver.dMode) {
             case POWER:
