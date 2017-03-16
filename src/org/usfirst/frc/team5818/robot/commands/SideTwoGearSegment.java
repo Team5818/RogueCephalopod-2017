e93790c5fda5b5733905612aa8ba1c785aaa6d89
Petey <pdotjpr@gmail.com>
2017-03-16 08:23:11 -0700

    Bigger turret collection

--- src/org/usfirst/frc/team5818/robot/commands/SideTwoGearSegment.java
+++ src/org/usfirst/frc/team5818/robot/commands/SideTwoGearSegment.java
@@ -111,7 +111,7 @@ public class SideTwoGearSegment extends CommandGroup {
 
         if (extra == AutoExtra.COLLECT) {
             whileDriving.addSequential((new SetArmAngle(Arm.COLLECT_POSITION)));
-            whileDriving.addSequential(new TurretSmallAdjustment(0.0));
+            whileDriving.addSequential(new SetTurretAngle(0.0));
             whileDriving.addSequential(new CollectGear(.5, 2));
         } else if (extra == AutoExtra.PLACE) {
             whileDriving.addSequential(new SetArmAngle(Arm.LOAD_POSITION));
