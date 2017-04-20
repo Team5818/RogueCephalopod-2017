51a82158b9daae08d612b73e307a058c66356c95
Petey <pdotjpr@gmail.com>
2017-04-20 13:10:09 -0500

    Redid side auto

--- src/org/usfirst/frc/team5818/robot/autos/ProfileSideGear.java
+++ src/org/usfirst/frc/team5818/robot/autos/ProfileSideGear.java
@@ -2,7 +2,9 @@ package org.usfirst.frc.team5818.robot.autos;
 
 import org.usfirst.frc.team5818.robot.commands.DriveTrajectory;
 import org.usfirst.frc.team5818.robot.commands.SpinWithProfile;
+import org.usfirst.frc.team5818.robot.commands.SpinWithProfileVision;
 import org.usfirst.frc.team5818.robot.commands.SpinWithVision;
+import org.usfirst.frc.team5818.robot.commands.TapeMode;
 import org.usfirst.frc.team5818.robot.commands.driveatratio.DriveAtRatio;
 import org.usfirst.frc.team5818.robot.commands.placewithlimit.PlaceWithLimit;
 import org.usfirst.frc.team5818.robot.constants.Camera;
@@ -24,24 +26,20 @@ public class ProfileSideGear extends CommandGroup{
         else{
             angleMult = 1;
         }
+        addSequential(new TapeMode());
         addSequential(new DriveTrajectory(70, 0.0, 0.0, 0.0, Direction.BACKWARD, true));
-        addSequential(new SpinWithVision(60, 20, Spin.CLOCKWISE, Camera.CAM_TAPE));
+        addSequential(new SpinWithProfile(angleMult*Math.toRadians(50.0), true, true));
+        addSequential(new SpinWithProfileVision(true, Camera.CAM_TAPE));
         addSequential(DriveAtRatio.withVision(Camera.CAM_TAPE, b -> {
-            b.inches(28);
+            b.inches(75);
             b.maxPower(.7);
             b.maxRatio(3.0);
             b.stoppingAtEnd(false);
         }));
-        addSequential(DriveAtRatio.withDeadReckon(b -> {
-            b.inches(8);
-            b.maxPower(.7);
-            b.targetRatio(1.0);
-            b.stoppingAtEnd(true);
-        }));
-        this.addSequential(new TimedCommand(1.0));
+        this.addSequential(new TimedCommand(.5));
         addSequential(new PlaceWithLimit());
-        addSequential(new DriveTrajectory(36, angleMult*Math.toRadians(60.0), 0.0, 0.0, Direction.FORWARD, true));
-        addSequential(new SpinWithProfile(Math.PI, true, false));
-        addSequential(new DriveTrajectory(70, Math.PI, 0.0, 0.0, Direction.FORWARD, true));
+        addSequential(new DriveTrajectory(28, angleMult*Math.toRadians(60.0), 0.0, 0.0, Direction.FORWARD, true));
+        addSequential(new SpinWithProfile(angleMult*Math.PI, true, false));
+        addSequential(new DriveTrajectory(320, angleMult*Math.PI, 0.0, 0.0, Direction.FORWARD, true));
     }
 }
