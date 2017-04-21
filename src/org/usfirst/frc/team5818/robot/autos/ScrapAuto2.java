efbda26c9edf3eb465d81a32c92f316bbff01e79
Petey <pdotjpr@gmail.com>
2017-04-21 08:27:46 -0500

    Scrapos for El Chapo

--- src/org/usfirst/frc/team5818/robot/autos/ScrapAuto2.java
+++ src/org/usfirst/frc/team5818/robot/autos/ScrapAuto2.java
@@ -1,22 +1,23 @@
 package org.usfirst.frc.team5818.robot.autos;
 
-import org.usfirst.frc.team5818.robot.commands.SpinUntilTargetInFOV;
+import org.usfirst.frc.team5818.robot.Robot;
 import org.usfirst.frc.team5818.robot.commands.SpinWithProfile;
 import org.usfirst.frc.team5818.robot.commands.SpinWithProfileVision;
-import org.usfirst.frc.team5818.robot.commands.SpinWithVision;
 import org.usfirst.frc.team5818.robot.commands.TapeMode;
 import org.usfirst.frc.team5818.robot.commands.driveatratio.DriveAtRatio;
 import org.usfirst.frc.team5818.robot.constants.Camera;
 import org.usfirst.frc.team5818.robot.constants.Spin;
+import org.usfirst.frc.team5818.robot.subsystems.VisionTracker;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.TimedCommand;
 
-public class ScrapAuto extends CommandGroup {
+public class ScrapAuto2 extends CommandGroup {
 
-    DriveAtRatio visionSpinArea;
+    Command visionSpinArea;
+    private final VisionTracker vision = Robot.runningRobot.vision;
     
-    public ScrapAuto() {
+    public ScrapAuto2() {
 //        addSequential(new SpinWithVision(60, 20, Spin.CLOCKWISE, Camera.CAM_TAPE));
 //        addSequential(DriveAtRatio.withSpin(b -> {
 //            b.angle(5);
@@ -26,8 +27,24 @@ public class ScrapAuto extends CommandGroup {
 //        }));
         addSequential(new TapeMode());
         addSequential(new SpinWithProfile(Math.toRadians(40.0), true, true));
-        addSequential(new SpinWithProfileVision(true, Camera.CAM_TAPE));
+        addSequential(visionSpinArea = DriveAtRatio.withSpin(b -> {
+            b.angle(20);
+            b.rotation(Spin.CLOCKWISE);
+            b.maxPower(.5);
+            b.stoppingAtEnd(false);
+        }));
         //addSequential(new SpinWithProfileVision(true, Camera.CAM_TAPE));
-        
+        //addSequential(new SpinWithProfileVision(true, Camera.CAM_TAPE));    
+    }
+    
+    @Override
+    protected boolean isFinished() {
+        if (visionSpinArea.isRunning()) {
+            double a = vision.getCurrentAngle();
+            if (!Double.isNaN(a) && Math.abs(a) < 5) {
+                return true;
+            }
+        }
+        return super.isFinished();
     }
 }
