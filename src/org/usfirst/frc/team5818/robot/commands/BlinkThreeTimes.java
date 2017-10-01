6ee51b189ea97f181415e0a3031d391a7fd0b2a2
Petey <pdotjpr@gmail.com>
2017-09-30 19:42:22 -0700

    aint loops nifty?

--- src/org/usfirst/frc/team5818/robot/commands/BlinkThreeTimes.java
+++ src/org/usfirst/frc/team5818/robot/commands/BlinkThreeTimes.java
@@ -22,21 +22,15 @@ package org.usfirst.frc.team5818.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.TimedCommand;
 
-public class BlinkThreeTimes extends CommandGroup{
+public class BlinkThreeTimes extends CommandGroup {
 
     public BlinkThreeTimes() {
-        this.addSequential(new LightsOn(true));
-        this.addSequential(new TimedCommand(.07));
-        this.addSequential(new LightsOn(false));
-        this.addSequential(new TimedCommand(.04));        
-        this.addSequential(new LightsOn(true));
-        this.addSequential(new TimedCommand(.07));
-        this.addSequential(new LightsOn(false));
-        this.addSequential(new TimedCommand(.07));
-        this.addSequential(new LightsOn(true));
-        this.addSequential(new TimedCommand(.07));
-        this.addSequential(new LightsOn(true));
-        this.addSequential(new TimedCommand(.04));
-        this.addSequential(new LightsOn(false));
+        for (int i = 0; i < 3; i++) {
+            this.addSequential(new TimedCommand(.04));
+            this.addSequential(new LightsOn(true));
+            this.addSequential(new TimedCommand(.07));
+            this.addSequential(new LightsOn(false));
+
+        }
     }
 }
